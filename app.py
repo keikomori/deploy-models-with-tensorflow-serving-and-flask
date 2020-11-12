217497226623c1f6cb08b68f513508b93882e2a2
Keiko <tatianymori@gmail.com>
2020-11-12 10:46:18 -0300

    display results

--- app.py
+++ app.py
@@ -17,11 +17,16 @@ def index():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
-            image_path = os.path.join('static', uploaded_file.filename)
+            image_path = os.path.join('C:\\Users\\MOGO\\Documents\\Tatiany\\python\\courses\\deploy-models-with-tensorflow-serving-and-flask\\static', uploaded_file.filename)
             uploaded_file.save(image_path)
             class_name = inference.get_prediction(image_path)
             print('CLASS NAME=', class_name)
             
+            result ={
+                'class_name': class_name,
+                'image_path': image_path,
+            }
+            return render_template('show.html', result=result)
     return render_template('index.html')
 
 if __name__ == '__main__':
