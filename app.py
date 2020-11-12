fb986b70be856f2e9cdbd96a9981a756c6499e88
Keiko <tatianymori@gmail.com>
2020-11-12 10:27:06 -0300

    connect the model server to the app

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_bootstrap import Bootstrap
+
+import os
+import inference
+
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -7,10 +11,17 @@ Bootstrap(app)
 """
 Routes
 """
-
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.join('static', uploaded_file.filename)
+            uploaded_file.save(image_path)
+            class_name = inference.get_prediction(image_path)
+            print('CLASS NAME=', class_name)
+            
     return render_template('index.html')
 
 if __name__ == '__main__':
--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ import requests
 
 SIZE = 128
 MODEL_URI = 'http://localhost:8501/v1/models/pets:predict'
+CLASSES = ['Cat', 'Dog']
 
 def get_prediction(image_path):
     image = tf.keras.preprocessing.image.load_img(
@@ -20,7 +21,8 @@ def get_prediction(image_path):
 
     response = requests.post(MODEL_URI, data=data.encode())
     result = json.loads(response.text)
+    print(result)
     prediction =np.squeeze(result['predictions'][0])
     class_name = CLASSES[int(prediction > 0.5)]
-
-get_prediction('dog.jpg')
+    
+    return class_name
